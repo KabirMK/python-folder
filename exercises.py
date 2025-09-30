4dd1bc50a39a62025cc01844378a76422b978cd0
KM Kabir <kayeemmkay@gmail.com>
2025-09-30 12:03:29 +0100

    Bug fix

--- exercises.py
+++ exercises.py
@@ -9,16 +9,22 @@
 
 # code:
 
-import random
-while True:
-    user_input = input(
-        "Do you want to roll the dice? (yes/no or y/n):").lower().strip()
-    if user_input == 'yes' or user_input == 'y':
-        dice1 = random.randint(1, 6)
-        dice2 = random.randint(1, 6)
-        print(f"You rolled a {dice1} and a {dice2}.")
-    elif user_input == 'no' or user_input == 'n':
-        print("Try again next time!")
-        break
-    else:
-        print("Invalid choice!")
+# import random
+# while True:
+#     user_input = input(
+#         "Do you want to roll the dice? (yes/no or y/n):").lower().strip()
+#     if user_input == 'yes' or user_input == 'y':
+#         dice1 = random.randint(1, 6)
+#         dice2 = random.randint(1, 6)
+#         print(f"You rolled a {dice1} and a {dice2}.")
+#     elif user_input == 'no' or user_input == 'n':
+#         print("Thank you for playing!")
+#         break
+#     else:
+#         print("Invalid choice!")
+
+
+user_input = input('input anything please:')
+reversed_sentence = user_input.split()[::-1]
+reversed_sentence = ' '.join(reversed_sentence)
+print(f'reversed senetnce is:, {reversed_sentence}')
